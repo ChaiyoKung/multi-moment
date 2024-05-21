f707dfc599244304fd46e4d7271ea3aeba0cf40c
ChaiyoKung <kananek45@gmail.com>
2024-05-22 01:30:18 +0700

    Refactor `formatDuration` function and add jsdoc

--- src/libs/dayjs.ts
+++ src/libs/dayjs.ts
@@ -3,7 +3,14 @@ import duration from "dayjs/plugin/duration";
 
 dayjs.extend(duration);
 
-export function formatDuration(seconds: number) {
-  const duration = dayjs.duration(seconds, "seconds");
+/**
+ * Formats a given time duration into "HH:mm:ss" format.
+ *
+ * @param time - The time duration to format.
+ * @param [unit='seconds'] - The unit of the time duration (e.g., 'seconds', 'minutes', 'hours').
+ * @returns The formatted time duration as "HH:mm:ss".
+ */
+export function formatDuration(time: number, unit: duration.DurationUnitType = "seconds") {
+  const duration = dayjs.duration(time, unit);
   return duration.format("HH:mm:ss");
 }
